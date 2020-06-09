2c35399b25a9ad6a735ecc7d1842c4eaefc7fc8a
Andrew Sherepenko <andrew.sherepenko@gmail.com>
2020-06-09 22:34:19 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    `java-gradle-plugin`
+    `kotlin-dsl`
+    kotlin("jvm") version "1.3.72"
+    id("com.gradle.plugin-publish") version "0.12.0"
+}
+
+group = "com.sherepenko"
+version = "0.1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+pluginBundle {
+    website = "https://asherepenko.github.io/build-version-plugin/"
+    vcsUrl = "https://github.com/asherepenko/build-version-plugin"
+    tags = listOf("semantic versioning", "software build version")
+}
+
+gradlePlugin {
+    plugins {
+        create("buildVersionPlugin") {
+            id = "com.sherepenko.gradle.version"
+            displayName = "Semantic Versioning Plugin"
+            description = "This Gradle plugin provides Semantic Versioning 2.0.0 implementation"
+            implementationClass = "com.sherepenko.gradle.version.BuildVersionPlugin"
+        }
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
